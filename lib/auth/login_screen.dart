3e208d338e205ac91952c1098ce20f9cb5f77bf3
Umair Suhaimee <umair1211@gmail.com>
2024-11-17 14:55:33 +0800

    Added some text form widgets

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -8,6 +8,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  bool rememberme = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +20,57 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset('MemberLink-Icon-512.png'),
+              const Text(style: TextStyle(fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold), "My Member Link"),
+              //Image.asset('MemberLink-Icon-512.png'),
               const SizedBox(height: 20),
-              
+              Padding(padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+              child: TextFormField(
+                controller: emailcontroller,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Email address",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email address';
+                  }
+                  return null;
+                },
+              ),),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                child: TextFormField(
+                  controller: passwordcontroller,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Password",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(value: rememberme, activeColor: Colors.blue, onChanged:(value) {
+                      rememberme = value!;
+                      setState(() {});
+                    },),
+                    const Text("Remember me"),
+                  ],
+                ),
+              )
             ],
           )
         )
       ),
     );
   }
+
 }
