556a3631e935f467d61c7185f8c5fb201f7a4ca4
Umair Suhaimee <umair1211@gmail.com>
2024-11-18 01:03:50 +0800

    Modified register form to follow logic

--- lib/auth/register_screen.dart
+++ lib/auth/register_screen.dart
@@ -8,100 +8,138 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
   TextEditingController emailcontroller = TextEditingController();
   TextEditingController passwordcontroller = TextEditingController();
   TextEditingController confirmpasswordcontroller = TextEditingController();
+  RegExp get _emailRegex => RegExp(r'^\S+@\S+$');
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(style: TextStyle(fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold), "My Member Link"),
-              //Image.asset('MemberLink-Icon-512.png'),
-              const SizedBox(height: 20),
-              Padding(padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-              child: TextFormField(
-                controller: emailcontroller,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Email address",
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email address';
-                  }
-                  return null;
-                },
-              ),),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                child: TextFormField(
-                  controller: passwordcontroller,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Password",
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: TextFormField(
-                  controller: confirmpasswordcontroller,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Confirm password",
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your confirm password';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                child: MaterialButton(
-                  elevation: 5,
-                  minWidth: 400,
-                  height: 50,
-                  onPressed: onRegister,
-                  color: Colors.blue,
-                  child: const Text(
-                    style: TextStyle(color: Colors.white),
-                    "Register"),
-                  ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      onTap: haveAccount,
-                      child: const Text(style: TextStyle(color: Colors.blue), "Already have an account?"),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: emailcontroller,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      labelText: 'Email address',
+                      suffixIcon: emailcontroller.text.isNotEmpty
+                      ? IconButton(onPressed: () {
+                        emailcontroller.clear();
+                      }, icon: const Icon(Icons.clear),)
+                      : null,
                     ),
-                  ],
-                ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email address';
+                      }
+                      else if (!_emailRegex.hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20,),
+                  TextFormField(
+                    controller: passwordcontroller,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      else if (value.length < 8) {
+                        return 'Password must be at least 8 characters long';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        password = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 20,),
+                  TextFormField(
+                    controller: confirmpasswordcontroller,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Confirm password',
+                      errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please confirm your password';
+                      }
+                      else if (value != password) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20,),
+                  MaterialButton(
+                    onPressed: onRegister,
+                    elevation: 5,
+                    minWidth: 400,
+                    height: 50,
+                    color: Colors.blue,
+                    child: const Text(style: TextStyle(
+                      color: Colors.white,
+                    ), 'Register'),
+                  ),
+                  const SizedBox(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(style: TextStyle(color: Colors.blue), 'Already have an account? Login'),
+                      ),
+                    ],
+                  )
+                ],
               ),
-            ],
-          )
-        )
-      ),
-    );
+            ),
+          ),
+          ),
+        ),
+      );
   }
 
   void onRegister() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text(style: TextStyle(color: Colors.white), 'Processing'),
+        backgroundColor: Colors.green[700],
+        duration: const Duration(seconds: 2),
+      ));
+    }
+    else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text(style: TextStyle(color: Colors.white), 'Please fill out all fields correctly'),
+        backgroundColor: Colors.red,
+        duration: Duration(seconds: 2),
+      ));
+    }
   }
 
   void haveAccount() {
