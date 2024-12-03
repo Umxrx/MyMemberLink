6b1d34585541a35839649c43e3169f389238e3c2
Umair Suhaimee <umair1211@gmail.com>
2024-12-03 16:46:22 +0800

    Modified php files

--- lib/server/memberlink/api/update_news.php
+++ lib/server/memberlink/api/update_news.php
@@ -1,5 +1,4 @@
 <?php
-
 if (!isset($_POST)) {
 	$response = array('status' => 'failed', 'data' => null);
     sendJsonResponse($response);
@@ -7,21 +6,20 @@ if (!isset($_POST)) {
 }
 
 include_once("dbconnect.php");
-$username = $_POST['username'];
-$email = $_POST['email'];
-$userphone = $_POST['userphone'];
-$password = sha1($_POST['password']);
+$newsid = $_POST['newsid'];
+$title = addslashes($_POST['title']);
+$details =addslashes( $_POST['details']);
 
-$sqlinsert="INSERT INTO `user_tbl`(`user_name`, `user_email`, `user_phone`, `user_password`) VALUES ('$username', '$email', '$userphone','$password');";
+$sqlupdatenews="UPDATE `news_tbl` SET `news_title`='$title',`news_details`='$details' WHERE `news_id` = '$newsid'";
 
-if ($conn->query($sqlinsert) === TRUE) {
+if ($conn->query($sqlupdatenews) === TRUE) {
 	$response = array('status' => 'success', 'data' => null);
     sendJsonResponse($response);
-}
-else {
+}else{
 	$response = array('status' => 'failed', 'data' => null);
 	sendJsonResponse($response);
 }
+	
 	
 
 function sendJsonResponse($sentArray)
--- lib/server/memberlink/api/user_login.php
+++ lib/server/memberlink/api/user_login.php
@@ -8,21 +8,19 @@ if (!isset($_POST)) {
 
 include_once("dbconnect.php");
 $email = $_POST['email'];
+$password = sha1($_POST['password']);
 
-// Prepare and execute SQL query to check if the email exists in `tbl_users`
-$sqlcheck = "SELECT * FROM `user_tbl` WHERE `user_email` = ?";
-$stmt = $conn->prepare($sqlcheck);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
+// SQL query to check if a user with the provided email and hashed password exists in `tbl_admins`
+$sqllogin = "SELECT `user_email`, `user_password` FROM `user_tbl` WHERE `user_email` = '$email' AND `user_pass` = '$password'";
+$result = $conn->query($sqllogin); // Execute the query
 
 // Check if any rows were returned (i.e., if the email exists)
 if ($result->num_rows > 0) {
     // Email already exists
-    $response = array('status' => 'success', 'data' => 'Email already registered');
+    $response = array('status' => 'success', 'data' => null);
 } else {
     // Email does not exist
-    $response = array('status' => 'failed', 'data' => 'Email is available');
+    $response = array('status' => 'failed', 'data' => null);
 }
 
 sendJsonResponse($response);
