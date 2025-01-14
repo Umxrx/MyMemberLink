ca0f28f5110f255e20b8539ccb6d6ab095cdf477
Umair Suhaimee <umair1211@gmail.com>
2025-01-15 04:23:21 +0800

    Modified Membership Model

--- lib/model/membership.dart
+++ lib/model/membership.dart
@@ -3,18 +3,21 @@ class Membership {
   String? membershipName;
   String? membershipDescription;
   String? membershipPrice;
+  String? membershipDuration;
 // name, picture, description, quantity, price, etc
   Membership(
       {this.membershipId,
       this.membershipName,
       this.membershipDescription,
-      this.membershipPrice,});
+      this.membershipPrice,
+      this.membershipDuration,});
 
   Membership.fromJson(Map<String, dynamic> json) {
     membershipId = json['membership_id'];
     membershipName = json['membership_name'];
     membershipDescription = json['membership_description'];
     membershipPrice = json['membership_price_RM'];
+    membershipDuration = json['membership_duration_month'];
   }
 
   Map<String, dynamic> toJson() {
@@ -23,6 +26,7 @@ class Membership {
     data['membership_name'] = membershipName;
     data['membership_description'] = membershipDescription;
     data['membership_price_RM'] = membershipPrice;
+    data['membership_duration_month'] = membershipDuration;
     return data;
   }
 }
